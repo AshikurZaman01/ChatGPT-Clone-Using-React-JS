3caf16be7f2cb1352b1893a2f844c94779a3ba65
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-03-10 15:49:07 +0600

    add upper Sidebar

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,8 +1,15 @@
+import SideBar from "../Sidebar/SideBar";
 
 const Home = () => {
     return (
-        <div>
-            this is home
+        <div className="min-h-[100vh] grid grid-cols-12">
+
+            <div className="border col-span-3  border-white">
+                <SideBar></SideBar>
+            </div>
+
+            <div className="main border col-span-9 border-red-400">cc</div>
+
         </div>
     );
 };
