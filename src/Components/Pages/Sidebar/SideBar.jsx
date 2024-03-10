673a6774612188ee0d6b2c9e51e81c4e5071ad92
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-03-10 22:44:33 +0600

    update some data

--- src/Components/Pages/Sidebar/SideBar.jsx
+++ src/Components/Pages/Sidebar/SideBar.jsx
@@ -4,32 +4,31 @@ import { LuMessageSquare } from "react-icons/lu";
 
 const SideBar = () => {
     return (
-        <div>
-
-            <div className="sideBar">
-
-                <div className="upperSide flex flex-col my-[1rem] mx-[16px] space-y-5">
-
-                    <div className="upperSideTop space-y-5">
-
-                        <div className="flex  items-center gap-3">
-                            <img className="logo" src={logo} alt="" />
-                            <h1 className="text-3xl">ChatGtp</h1>
-                        </div>
-
-                        <button className="midBtn flex justify-center btn btn-md btn-primary w-full"><span><FiPlus className="font-bold text-xl" /></span> New Chat</button>
+        <div className="sideBar flex flex-col justify-between h-screen">
+            <div className="upperSide flex flex-col my-[1rem] mx-[16px] space-y-5">
+                <div className="upperSideTop space-y-5">
+                    <div className="flex items-center gap-3">
+                        <img className="logo" src={logo} alt="" />
+                        <h1 className="text-3xl">ChatGtp</h1>
                     </div>
-
-                    <div className="upperSideTop flex flex-col pt-6 space-y-4">
-                        <button className="query btn btn-md bg-transparent text-white  hover:bg-primary gap-5 items-center flex"><span><LuMessageSquare className="text-xl" /></span> What is programming ?</button>
-
-                        <button className="query btn btn-md bg-transparent text-white  hover:bg-primary gap-5 items-center flex"><span><LuMessageSquare className="text-xl" /></span> How To Use API ?</button>
-                    </div>
+                    <button className="midBtn flex justify-center btn btn-md btn-primary w-full">
+                        <span><FiPlus className="font-bold text-xl" /></span> New Chat
+                    </button>
                 </div>
-
-                <div className="lowerSide">dd</div>
+                <div className="upperSideTop flex flex-col pt-6 space-y-4">
+                    <button className="query btn btn-md bg-transparent text-white hover:bg-primary gap-5 items-center flex">
+                        <span><LuMessageSquare className="text-xl" /></span> What is programming ?
+                    </button>
+                    <button className="query btn btn-md bg-transparent text-white hover:bg-primary gap-5 items-center flex">
+                        <span><LuMessageSquare className="text-xl" /></span> How To Use API ?
+                    </button>
+                </div>
             </div>
-
+           
+            <div className="lowerSide bg-red-300">
+                <div>ff</div>
+            </div>
+            
         </div>
     );
 };
